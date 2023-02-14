0bf60c385f9e43026b8c270c2331412f12fae564
pinkjagnie <olga.krzysztofik@gmail.com>
2023-02-14 10:53:40 +0100

    Styles for new version of Success comp. /download/

--- client/src/components/ui/Success.js
+++ client/src/components/ui/Success.js
@@ -16,7 +16,11 @@ const Success = (props) => {
           <img src="https://i.ibb.co/Lkn7rkG/thank-you-envelope.png" alt="thank-you-envelope" border="0" />
           <h1 className={styles.thanksTitle}>Thank you!</h1>
           <p className={styles.thanksText}>Your file has been send to our amazing database</p> 
-          <button className={styles.goBackButton} onClick={closeSuccessHandler}>Go back</button>
+          <p className={styles.downloadTitle}>You can now download that file or just go back</p>
+          <div className={styles.buttonsBox}>
+            <button className={styles.downloadButton}>Download</button>
+            <button className={styles.goBackButton} onClick={closeSuccessHandler}>Go back</button>
+          </div>
         </div>
       </div>
     </section>
